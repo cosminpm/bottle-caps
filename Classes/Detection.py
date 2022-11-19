965e92efae580a25108e314728be6790a0490935
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2022-11-19 01:10:38 +0100

    Moving get_cropped_squares

--- Classes/Detection.py
+++ Classes/Detection.py
@@ -1,25 +1,26 @@
 import numpy as np
 
+from Classes.SquareDetection import SquareDetection
 from aux_scripts import distance_between_two_points
 
-MIN_NUM_OF_CENTROIDS = 7
-MAX_DISTANCE = 50
+MIN_NUM_POINTS_IN_SQUARE = 7
+MAX_DISTANCE = 40
 
 
 class Detection:
     def __init__(self, pix_kps: set[tuple[int]]):
         self.pix_kps = pix_kps
-        self.squares = self.detect_squares()
+        self.squares = []
+        self.detect_centroids()
 
-    def detect_squares(self):
-        squares = []
+    def detect_centroids(self):
         already_in_square = set()
         for origin_point in self.pix_kps:
             # p_X is on the horizontal axis and p_Y is on the vertical axis
             pMaxX, pMinX, pMaxY, pMinY = origin_point, origin_point, origin_point, origin_point
             already_in_square.add(origin_point)
 
-            centroid_list = []
+            points_list = []
             for p in self.pix_kps:
                 # Checks if the cap is in the range of the max caps
                 if (distance_between_two_points(pMaxX, p) < MAX_DISTANCE
@@ -28,7 +29,7 @@ class Detection:
                     or distance_between_two_points(pMinY, p) < MAX_DISTANCE) \
                         and p not in already_in_square:
                     already_in_square.add(p)
-                    centroid_list.append(p)
+                    points_list.append(p)
                     # Check horizontally
                     if p[0] > pMaxX[0]:
                         pMaxX = p
@@ -39,22 +40,17 @@ class Detection:
                         pMaxY = p
                     elif p[1] < pMinY[1]:
                         pMinY = p
-            if len(centroid_list) > MIN_NUM_OF_CENTROIDS:
+            if len(points_list) > MIN_NUM_POINTS_IN_SQUARE:
                 pTopLeft = (pMinX[0], pMaxY[1])
                 pBotRight = (pMaxX[0], pMinY[1])
-                centroid = np.array(centroid_list).mean(axis=0)
+                centroid = np.array(points_list).mean(axis=0)
                 centroid = tuple([int(i) for i in centroid])
                 dis = max(distance_between_two_points(centroid, pTopLeft),
                           distance_between_two_points(centroid, pBotRight))
-                squares.append([centroid, dis])
-        return squares
+                self.squares.append(SquareDetection(centroid, points_list, dis))
 
     def get_cropped_squares(self, img: np.array):
         croppeds = []
         for s in self.squares:
-            top = (int(abs(s[0][0] - s[1])), int(s[0][1] - s[1]))
-            bot = (int(s[0][0] + s[1]), int(abs(s[0][1] + s[1])))
-            h, w = bot[1] - top[1], bot[0] - top[0]
-
-            croppeds.append([img[top[1]:top[1] + h, top[0]:top[0] + w].copy(), top, bot])
+            croppeds.append(s.get_cropped_img(img))
         return croppeds
--- Classes/SquareDetection.py
+++ Classes/SquareDetection.py
@@ -1,8 +1,18 @@
+import numpy as np
 
 
 class SquareDetection:
-    def __init__(self):
-        pass
+    def __init__(self, centroid: tuple[int], points: list[tuple[int]], distance: int):
+        self.centroid = centroid
+        self.points = points
+        self.distance = distance
+
+    def get_cropped_img(self, img: np.array):
+        top = int(abs(self.centroid[0] - self.distance)), int(self.centroid[1] - self.distance)
+        bot = int(self.centroid[0] + self.distance), int(abs(self.centroid[1] + self.distance))
+        h, w = bot[1] - top[1], bot[0] - top[0]
+        return [img[top[1]:top[1] + h, top[0]:top[0] + w], top, bot]
 
     def get_if_match(self):
+        # TODO
         pass
