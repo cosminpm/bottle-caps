7bb7ddbb7802e952e6acd2dc4c38a0a0666fc02f
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2022-11-21 22:12:47 +0100

    Refactor made on DetectionManager

--- Classes/DetectionManager.py
+++ Classes/DetectionManager.py
@@ -1,6 +1,57 @@
-class DetectionManager:
-    def __init__(self):
-        self.detections = {}
-        self.photo_image = {}
+import os
 
-    def add_detection(self):
+import numpy as np
+
+from Classes.Detection import Detection
+from aux_scripts import read_img
+from kp_and_descriptors import compare_two_imgs
+
+MY_CAPS_IMGS_FOLDER = "./caps_imgs/"
+
+
+def get_detection(cap_img: np.ndarray, photo_img: np.ndarray, name_cap) -> Detection or None:
+    pix_kps = compare_two_imgs(img_cap=cap_img, img_photo=photo_img)
+    detection = Detection(pix_kps, name_cap, photo_img)
+    if len(detection.squares) > 0:
+        return detection
+    return None
+
+
+class DetectionManager:
+    def __init__(self, photo_image):
+        self.detections = {}
+        self.photo_image = photo_image
+        self.get_all_detections(self.photo_image)
+
+    def get_all_detections(self, photo_img: np.ndarray):
+        entries = os.listdir(MY_CAPS_IMGS_FOLDER)
+        for name_img in entries:
+            cap_str = MY_CAPS_IMGS_FOLDER + name_img
+            cap_img = read_img(cap_str)
+            detection = get_detection(cap_img, photo_img, name_img)
+            if detection:
+                self.detections[cap_str] = detection
+
+    def set_prng_match(self):
+        max_matches = self.get_max_matches()
+        for key in self.detections:
+            self.detections[key].set_prng_match(max_matches + 1)
+
+    def get_max_matches(self) -> int:
+        max_matches = 0
+        for key in self.detections:
+            max_matches = max(max_matches, self.detections[key].get_max_matches())
+        return max_matches
+
+    # Draw
+    def draw_squares_detections(self):
+        for key in self.detections:
+            self.photo_image = self.detections[key].draw_all_squares(self.photo_image)
+
+    def draw_percentage(self):
+        for key in self.detections:
+            self.photo_image = self.detections[key].draw_percentage(self.photo_image)
+
+    def draw_name(self):
+        for key in self.detections:
+            self.photo_image = self.detections[key].draw_name(self.photo_image)
--- main.py
+++ main.py
@@ -3,78 +3,26 @@ import numpy as np
 import os
 
 from Classes.Detection import Detection
+from Classes.DetectionManager import DetectionManager
 from aux_scripts import read_img
 from kp_and_descriptors import compare_two_imgs
 
-MY_CAPS_IMGS_FOLDER = "./caps_imgs/"
 
 def look_in_all_images(photo_str: str):
     photo_img = read_img(photo_str)
-    detections = get_all_detections(photo_img)
-    set_prng_match(detections)
+    det_manager = DetectionManager(photo_img)
+    det_manager.set_prng_match()
 
-    photo_img = draw_squares_detections(detections, photo_img)
-    photo_img = draw_percentage(detections, photo_img)
-    photo_img = draw_name(detections, photo_img)
+    det_manager.draw_squares_detections()
+    det_manager.draw_percentage()
+    det_manager.draw_name()
 
-    cv2.imshow("Result", photo_img)
+    cv2.imshow("Result", det_manager.photo_image)
     cv2.waitKey(0)
-
-
-def get_all_detections(photo_img: np.ndarray) -> list[Detection]:
-    entries = os.listdir(MY_CAPS_IMGS_FOLDER)
-    detections = []
-    for name_img in entries:
-        cap_str = MY_CAPS_IMGS_FOLDER + name_img
-        cap_img = read_img(cap_str)
-        detection = get_detection(cap_img, photo_img, name_img)
-        if detection:
-            detections.append(detection)
-    return detections
 
 
 def remove_overlapping_detections():
     pass
-
-
-def get_detection(cap_img: np.ndarray, photo_img: np.ndarray, name_cap) -> Detection or None:
-    pix_kps = compare_two_imgs(img_cap=cap_img, img_photo=photo_img)
-    detection = Detection(pix_kps, name_cap, photo_img)
-    if len(detection.squares) > 0:
-        return detection
-    return None
-
-
-def set_prng_match(detections: list[Detection]):
-    max_matches = get_max_matches(detections)
-    for detection in detections:
-        detection.set_prng_match(max_matches + 1)
-
-
-def get_max_matches(detections: list[Detection]) -> int:
-    max_matches = 0
-    for detection in detections:
-        max_matches = max(max_matches, detection.get_max_matches())
-    return max_matches
-
-
-# Draw
-def draw_squares_detections(detections: list[Detection], img: np.ndarray):
-    for detection in detections:
-        img = detection.draw_all_squares(img)
-    return img
-
-
-def draw_percentage(detections: list[Detection], img: np.ndarray):
-    for detection in detections:
-        img = detection.draw_percentage(img)
-    return img
-
-
-def draw_name(detections: list[Detection], img: np.ndarray):
-    for detection in detections:
-        img = detection.draw_name(img)
-    return img
 
 
 def get_value_const():
