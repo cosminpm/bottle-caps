16bafd814d0990001d212b85cc3397ca4184abe4
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2023-01-29 16:00:03 +0100

    Removing unnecessary

--- Scripts/aux_scripts.py
+++ Scripts/aux_scripts.py
@@ -11,39 +11,9 @@ DEBUG_BLOB = False
 MY_CAPS_IMGS_FOLDER = r"caps-s3"
 DATABASE_FODLER = r"caps_db-s3"
 
-# MY_CAPS_IMGS_FOLDER = r"resized_caps_imgs"
-# DATABASE_FODLER = r"caps_db"
-
-def find_dominant_color(img: np.ndarray) -> tuple[int, int, int]:
-    colors = {}
-    for pix in img[0]:
-        pix = tuple(pix)
-        pix = (pix[0] // 2, pix[1] // 2, pix[2] // 2)
-        if pix not in colors:
-            colors[pix] = 1
-        else:
-            colors[pix] += 1
-    dominant = max(colors, key=colors.get)
-    return tuple((int(dominant[0] * 2), int(dominant[1] * 2), int(dominant[2] * 2)))
-
-
-def compare_if_same_color(c1: np.ndarray, c2: np.ndarray, ratio: float) -> bool:
-    sum_numb_1 = c1[0] / (255 * 3) + c1[1] / (255 * 3) + c1[2] / (255 * 3)
-    sum_numb_2 = c2[0] / (255 * 3) + c2[1] / (255 * 3) + c2[2] / (255 * 3)
-    return abs(sum_numb_1 - sum_numb_2) > ratio
-
-
-def distance_between_two_points(p1: tuple, p2: tuple) -> float:
-    dist = ((abs(p1[0] - p2[0]) ** 2) + (abs(p1[1] - p2[1]) ** 2)) ** 0.5
-    return dist
-
 
 def read_img(img_path: str) -> np.ndarray:
     return cv2.cvtColor(cv2.imread(img_path), 1)
-
-
-def get_mid_point(p1: tuple[int], p2: tuple[int]) -> tuple[int, int]:
-    return tuple((int((p1[0] + p2[0]) / 2), int((p1[1] + p2[1]) / 2)))
 
 
 def rgb_to_bgr(rgb: tuple[int, int, int]) -> tuple[int, int, int]:
@@ -83,13 +53,6 @@ def resize_all_images(path, output, size):
     files = os.listdir(path)
     for file in files:
         resize_img_pix_with_name(path + file, output, size)
-
-
-def get_number_of_caps_in_image(path_to_image: str):
-    img = cv2.imread(path_to_image, 0)
-    _, avg_size = get_avg_size_all_blobs(img.copy())
-    _, circles = hough_transform_circle(img, avg_size)
-    return len(circles)
 
 
 def crate_db_for_cap(cap_name, folder: str):
