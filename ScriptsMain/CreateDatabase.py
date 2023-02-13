84da9f0a0fd593f1278263dae9660592d425b31b
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2023-02-14 00:00:50 +0100

    Update CreateDatabase.py

--- ScriptsMain/CreateDatabase.py
+++ ScriptsMain/CreateDatabase.py
@@ -108,58 +108,5 @@ def create_json_for_all_caps():
 
 
 
-def plot_colors(hist, centroids):
-    # initialize the bar chart representing the relative frequency
-    # of each of the colors
-    bar = np.zeros((50, 300, 3), dtype="uint8")
-    startX = 0
-    # loop over the percentage of each cluster and the color of
-    # each cluster
-    for (percent, color) in zip(hist, centroids):
-        # plot the relative percentage of each cluster
-        endX = startX + (percent * 300)
-        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
-                      color.astype("uint8").tolist(), -1)
-        startX = endX
-
-    # return the bar chart
-    return bar
-
-
-def centroid_histogram(clt):
-    # grab the number of different clusters and create a histogram
-    # based on the number of pixels assigned to each cluster
-    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
-    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
-    # normalize the histogram, such that it sums to one
-    hist = hist.astype("float")
-    hist /= hist.sum()
-    # return the histogram
-    return hist
-
-
-def get_all_rgb_images():
-    path = r"C:\Users\cosmi\Desktop\BottleCaps\database\caps-s3\cap-51.jpg"
-
-    org_img = cv2.imread(path)
-    img = cv2.cvtColor(org_img, cv2.COLOR_BGR2RGB)
-    img = org_img.reshape((img.shape[0] * img.shape[1], 3))
-    clt = KMeans(n_clusters=3)
-    clt.fit(img)
-
-    hist = centroid_histogram(clt)
-    bar = plot_colors(hist, clt.cluster_centers_)
-    # show our color bart
-    cv2.imshow("imagen",org_img)
-    plt.axis("off")
-    plt.imshow(bar)
-    plt.show()
-
-
-def main_rgb():
-    get_all_rgb_images()
-
-
 if __name__ == '__main__':
-    main_rgb()
-    # create_json_for_all_caps()
+    create_json_for_all_caps()
