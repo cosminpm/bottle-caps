f81fadaf0979cd9ac19a445f91b6a1e47a551cd9
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2023-02-13 01:06:51 +0100

    Some mor etests

--- ScriptsMain/CreateDatabase.py
+++ ScriptsMain/CreateDatabase.py
@@ -4,6 +4,9 @@ import os
 import cv2
 import numpy as np
 from pathlib import Path
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
 
 DEBUG_BLOB = False
 MY_CAPS_IMGS_FOLDER = r"database/caps-s3"
@@ -104,20 +107,53 @@ def create_json_for_all_caps():
         crate_db_for_cap(name_img, path_caps)
 
 
-def average_color(path):
-    img = cv2.imread(path)
-    # Convert the image to the BGR color space
-    bgr = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
 
-    # Get the average color of the image
-    avg_color = np.average(np.average(bgr, axis=0), axis=0)
-    return int(avg_color[0]), int(avg_color[1]), int(avg_color[2])
+def plot_colors(hist, centroids):
+    # initialize the bar chart representing the relative frequency
+    # of each of the colors
+    bar = np.zeros((50, 300, 3), dtype="uint8")
+    startX = 0
+    # loop over the percentage of each cluster and the color of
+    # each cluster
+    for (percent, color) in zip(hist, centroids):
+        # plot the relative percentage of each cluster
+        endX = startX + (percent * 300)
+        cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+                      color.astype("uint8").tolist(), -1)
+        startX = endX
+
+    # return the bar chart
+    return bar
+
+
+def centroid_histogram(clt):
+    # grab the number of different clusters and create a histogram
+    # based on the number of pixels assigned to each cluster
+    numLabels = np.arange(0, len(np.unique(clt.labels_)) + 1)
+    (hist, _) = np.histogram(clt.labels_, bins=numLabels)
+    # normalize the histogram, such that it sums to one
+    hist = hist.astype("float")
+    hist /= hist.sum()
+    # return the histogram
+    return hist
 
 
 def get_all_rgb_images():
-    avg_color = average_color(r"C:\Users\cosmi\Desktop\BottleCaps\database\caps-s3\cap-54.jpg")
-    print(avg_color)
-    return avg_color
+    path = r"C:\Users\cosmi\Desktop\BottleCaps\database\caps-s3\Untitled.png"
+
+    org_img = cv2.imread(path)
+    img = cv2.cvtColor(org_img, cv2.COLOR_BGR2RGB)
+    img = org_img.reshape((img.shape[0] * img.shape[1], 3))
+    clt = KMeans(n_clusters=3)
+    clt.fit(img)
+
+    hist = centroid_histogram(clt)
+    bar = plot_colors(hist, clt.cluster_centers_)
+    # show our color bart
+    cv2.imshow("imagen",org_img)
+    plt.axis("off")
+    plt.imshow(bar)
+    plt.show()
 
 
 def main_rgb():
