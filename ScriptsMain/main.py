3b6a6106beba99244b00dc7c0efe5adfa2fa2992
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2023-05-06 15:42:23 +0200

    a

--- ScriptsMain/main.py
+++ ScriptsMain/main.py
@@ -10,6 +10,14 @@ app = FastAPI()
 # curl -X POST -F "file=@C:\Users\cosmi\Desktop\BottleCaps\database\test-images\test-i-have\5.jpg" http://127.1.0.2:8080/upload
 # C:\Users\cosmi\Desktop\BottleCaps\database\test-images\test-i-have\5.jpg
 
+# bottlecaps-production.up.railway.app
+# curl -X POST -F "file=@C:\Users\cosmi\Desktop\BottleCaps\database\test-images\test-i-have\5.jpg" bottlecaps-production.up.railway.app
+# RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+
+
+# Install command
+# apt-get update && apt-get install -y libopencv-dev && python ScriptsMain/main.py
+
 def process_image(path: str):
     image = read_img_from_path(path)
     cropped_images = detect_caps(image)
@@ -34,4 +42,4 @@ async def upload_file(file: UploadFile = File(...)):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.1.0.2", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
