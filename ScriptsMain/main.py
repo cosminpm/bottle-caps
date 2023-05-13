05a9fbd96faa31b5de85ca84bfc3aeb066d35866
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2023-05-13 21:17:16 +0200

    Test changes

--- ScriptsMain/main.py
+++ ScriptsMain/main.py
@@ -1,20 +1,15 @@
-import json
+import numpy as np
 import uvicorn
 
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+
+
 from DetectCaps import detect_caps
-from utilsFun import read_img_from_path
+from utilsFun import read_img_from_path, read_img_numpy
 
 # curl -X POST -F "file=@C:\Users\cosmi\Desktop\BottleCaps\database\test-images\test-i-have\5.jpg" http://127.1.0.2:8080/upload
 # C:\Users\cosmi\Desktop\BottleCaps\database\test-images\test-i-have\5.jpg
-
-# bottlecaps-production.up.railway.app
-# curl -X POST -F "file=@C:\Users\cosmi\Desktop\BottleCaps\database\test-images\test-i-have\5.jpg" bottlecaps-production.up.railway.app/upload
-# RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
-
-
-# Install command
-# apt-get update && apt-get install -y libopencv-dev && python ScriptsMain/main.py
 
 # Usefull
 # curl -X POST -F "file=@C:\Users\cosmi\Desktop\BottleCaps\database\test-images\test-i-have\5.jpg" https://bottlecaps-production.up.railway.app/upload
@@ -22,13 +17,27 @@ from utilsFun import read_img_from_path
 
 app = FastAPI()
 
-ASGFAWEfg
-def process_image(path: str):
-    image = read_img_from_path(path)
+origins = [
+    "*",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def process_image(file_contents: bytes):
+    image = np.frombuffer(file_contents, np.uint8)
+    image = read_img_numpy(image)
     cropped_images = detect_caps(image)
     return [tuple(int(v) for v in rct) for (img, rct) in cropped_images]
 
-
+alwkfhnlakwne
 @app.get("/")
 async def root():
     return {"message": "Welcome to the file upload API!"}
@@ -36,10 +45,9 @@ async def root():
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    result = process_image(file.filename)
-    json_result = json.dumps(result)
-    return {"filename": file.filename, "result": json_result}
+    result = process_image(await file.read())
+    return {"filename": file.filename, "result": result}
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="localhost", port=8080)
